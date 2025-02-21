fa46e967b213f6fede92db6cf2c5f74b153530b4
antoine <153529541+antoineswg@users.noreply.github.com>
2025-02-21 15:21:00 +0100

    fect api + affichage du mot sélectionné

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mot, setMot] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3333/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams({ locale: 'en-GB' })
+    })
+      .then(response => response.json())
+      .then(data => {
+        setMot(data.word);
+      })
+      .catch(error => console.error('Error:', error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>{mot}</div>
+  );
 }
 
-export default App
+export default App;
